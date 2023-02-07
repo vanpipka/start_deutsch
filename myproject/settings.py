0fde81f8bc04507bf0cabc5ce2c42b8afb765fb9
Andrei <vanpipka@gmail.com>
2023-02-07 17:46:56 +0300

    1

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-from settings import SECRET_KEY, DATABASES, ALLOWED_HOSTS
+from settings.constants import SECRET_KEY, DATABASES, ALLOWED_HOSTS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
