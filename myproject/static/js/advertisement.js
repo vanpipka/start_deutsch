136214b7a327eda1d4eab84aa9ba6cbc3c5f8b11
Andrei <vanpipka@gmail.com>
2023-02-10 11:49:48 +0300

    login

--- myproject/static/js/advertisement.js
+++ myproject/static/js/advertisement.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     //console.log(window.innerWidth)
     if (window.innerWidth <= 768) {
         document.getElementById("ya_rtb_4").hidden = false;
-        document.getElementById("adv").removeChild(document.getElementById("ya_rtb_1"));
+        document.getElementById("ya_rtb_1").remove();
     }
     else{
         document.getElementById("ya_rtb_1").hidden = false;
-        document.getElementById("adv").removeChild(document.getElementById("ya_rtb_4"));
+        document.getElementById("ya_rtb_4").remove();
     }
     }, false);
